76c3eaa8b26a15b96a207971edb6822a89ee3763
Ahmad Muahsien <ahmuhaisen03@gmail.com>
2024-08-30 15:56:46 +0300

    New Controller | VolunteersController (bare bone)

---
+++ Magazine.Api/Controllers/VolunteersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Magazine.Api.Controllers;
+
+[Route("api/[controller]")]
+public class VolunteersController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet]
+    [Route("{volunteerId:int}")]
+    public async Task<IActionResult> GetValue(int volunteerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet]
+    [Route("{volunteerId:int}/contributions")]
+    public async Task<IActionResult> GetVolunteerContributions(int volunteerId)
+    {
+        throw new NotImplementedException();
+    }
+}
